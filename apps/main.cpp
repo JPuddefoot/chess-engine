7bcef3e533e03e94a533a2472690cd40652e4dd7
Josh Puddefoot <joshua.eldon.puddefoot@cern.ch>
2021-05-18 19:54:13 +0100

    Changed main cpp

--- apps/main.cpp
+++ apps/main.cpp
@@ -9,32 +9,23 @@ int main() {
     Board board = Board();
 
     board.generateMoves();
-  //  for (Move move : board.moveList) {
-  //      std::cout << "origin: " << Square_array[static_cast<std::size_t>(move.origin)]
-  //          << "destination: " << Square_array[static_cast<std::size_t>(move.destination)]
- //           << "\n";
- //   }
 
     std::cout << board.printBoard();
-    std::cout << bitboard_to_string(board.white_pieces);
 
-    Move move = board.moveList[4];
-    std::cout << "Moving: " << Square_array[static_cast<std::size_t>(move.origin)];
-    board.makeMove(move.origin, move.destination);
-
-
-    board.generateMoves();
-    move = board.moveList[4];
-    std::cout << "Moving: " << Square_array[static_cast<std::size_t>(move.origin)];
-    board.makeMove(move.origin, move.destination);
+    board.makeMove(Square::E2, Square::E4);
+    board.makeMove(Square::D7, Square::D5);
 
     std::cout << board.printBoard();
 
     board.generateMoves();
-    move = board.moveList[9];
-    board.makeMove(move.origin, move.destination);
 
-    std::cout << board.printBoard();
+    std::cout << bitboard_to_string(board.whitePieces);
+
+    for (Move move : board.moveList) {
+      std::cout << "origin: " << Square_array[static_cast<std::size_t>(move.origin)]
+        << "destination: " << Square_array[static_cast<std::size_t>(move.destination)]
+        << "\n";
+    }
 
 
 
